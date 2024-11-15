562d71fc5043332084299b1b80685c9e2b005f13
AsterElend <j260197@johnsburg12.org>
2024-11-15 15:42:16 -0600

    First Commit

---
+++ src/main/java/aster/otherworld/mixin/OtherworldClient.java
@@ -0,0 +1,10 @@
+package aster.otherworld.mixin;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OtherworldClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
